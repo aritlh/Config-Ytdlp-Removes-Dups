c3fd0682c2c84af50d2704ed9adeeac4f3ac9e3e
norieau <a.arijogja@gmail.com>
2024-01-26 18:11:59 +0700

    remove srt and mp4, move only subbed file

--- embd.sh
+++ embd.sh
@@ -13,8 +13,11 @@ ffmpeg -i "$mp4" -i "$srt" -c:v copy -c:a copy -c:s mov_text "$base_name"_subbed
 
 # Move output file to finished folder
 mkdir -p ../finished
-mv *.mp4 ../finished
-mv *.srt ../finished
+mv *subbed.mp4 ../finished
+# mv *.srt ../finished
+
+rm -r *.srt
+rm -r *.mp4
 
 # Go back to parent folder
 cd ..
